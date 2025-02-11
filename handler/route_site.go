778379b91429b30c5b4732c74074474bad7ce869
gshikhar2021 <gshikhar84@gmail.com>
2025-02-12 01:27:14 +0530

    Add Handler Function

---
+++ handler/route_site.go
@@ -0,0 +1,54 @@
+package handler
+
+import (
+	"net/http"
+	"strings"
+	"github.com/gorilla/mux"
+)
+
+// GET /chats
+func listChats(w http.ResponseWriter, r *http.Request) {
+	rooms, err := data.CS.Chats()
+	if err != nil {
+		errorMessage(w, r, "Cannot retrieve chats")
+	} else {
+		// to return back to refreshing page:
+		//generateHTML(w, &rooms, "layout", "sidebar", "public.header", "list")
+		generateHTMLContent(w, &rooms, "list")
+		return
+	}
+}
+
+// GET /
+// Default page
+func index(w http.ResponseWriter, r *http.Request) {
+	generateHTML(w, "", "layout", "sidebar", "public.header", "index")
+}
+
+// GET /chat/<id>/chatbox
+// Default page
+func chatbox(w http.ResponseWriter, r *http.Request) {
+	queries := mux.Vars(r)
+	if titleOrID, ok := queries["titleOrID"]; ok {
+		cr, err := data.CS.Retrieve(titleOrID)
+		if err != nil {
+			Info("erroneous chats API request", r, err)
+		}
+		generateHTMLContent(w, &cr, "chat")
+	}
+}
+
+// GET /chats/<id>/entrance
+// Default page
+func joinRoom(w http.ResponseWriter, r *http.Request) (err error) {
+	queries := mux.Vars(r)
+	if titleOrID, ok := queries["titleOrID"]; ok {
+		cr, err := data.CS.Retrieve(titleOrID)
+		if err != nil {
+			Info("erroneous chats API request", r, err)
+			return err
+		}
+		generateHTML(w, (strings.ToLower(cr.Type) == data.PrivateRoom || cr.Type == data.HiddenRoom), "layout", "sidebar", "public.header", "entrance")
+	}
+	return
+}
---
+++ handler/utils.go
@@ -0,0 +1,85 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"reflect"
+	"runtime"
+)
+
+var logger *log.Logger
+
+/* Convenience function for printing to stdout
+func p(a ...interface{}) {
+	fmt.Println(a...)
+}*/
+
+// Info will log information with "INFO" prefix to logger
+func Info(args ...interface{}) {
+	logger.SetPrefix("INFO ")
+	logger.Println(args...)
+}
+
+// Danger will log information with "ERROR" prefix to logger
+func Danger(args ...interface{}) {
+	logger.SetPrefix("ERROR ")
+	logger.Println(args...)
+}
+
+// Warning will log information with "WARNING" prefix to logger
+func Warning(args ...interface{}) {
+	logger.SetPrefix("WARNING ")
+	logger.Println(args...)
+}
+
+// ReportStatus is a helper function to return a JSON response indicating outcome success/failure
+func ReportStatus(w http.ResponseWriter, success bool, err *data.APIError) {
+	var res *data.Outcome
+	w.Header().Set("Content-Type", "application/json")
+	if success {
+		res = &data.Outcome{
+			Status: success,
+		}
+	} else {
+		res = &data.Outcome{
+			Status: success,
+			Error:  err,
+		}
+	}
+	response, _ := json.Marshal(res)
+	if _, err := w.Write(response); err != nil {
+		Danger("Error writing", response)
+	}
+}
+
+func generateHTML(writer http.ResponseWriter, data interface{}, filenames ...string) {
+	var files []string
+	for _, file := range filenames {
+		files = append(files, fmt.Sprintf("templates/%s.html", file))
+	}
+	templates := template.Must(template.ParseFiles(files...))
+	if err := templates.ExecuteTemplate(writer, "layout", data); err != nil {
+		Danger("Error generating HTML template", data, err.Error())
+	}
+}
+
+func generateHTMLContent(writer http.ResponseWriter, data interface{}, file string) {
+	writer.Header().Set("Content-Type", "text/html")
+	t, _ := template.ParseFiles(fmt.Sprintf("templates/content/%s.html", file))
+	if err := t.Execute(writer, data); err != nil {
+		Danger("Error executing HTML template", data, err.Error())
+	}
+}
+
+// convenience function to be chained with another HandlerFunc
+// that prints to the console which handler was called.
+func logConsole(h http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
+		fmt.Println("Handler function called - " + name)
+		h(w, r)
+	}
+}
